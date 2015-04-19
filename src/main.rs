8dff5decb21baa1a0a1ee905f6cb94dc9916546d
Kyle Pink <kylepink@gmail.com>
2015-04-19 15:28:26 +0930

    error exit code now used

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(exit_status)]
+
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate rustfmt;
@@ -45,26 +47,32 @@ fn main() {
     let style = FormatStyle::default();
 
     if args.arg_file.is_empty() {
-        match format_stdin(style) {
+        match format_stdin(&style) {
             Ok(ref s)  => write!(&mut stdout(), "{}", s).unwrap(),
-            Err(ref s) => write!(&mut stderr(), "{}", s).unwrap(),
+            Err(ref s) => {
+                write!(&mut stderr(), "{}", s).unwrap();
+                std::env::set_exit_status(1);
+            },
         }
     } else {
         for name in args.arg_file {
             let path = Path::new(&name);
-            match format_source_file(style.clone(), path) {
+            match format_source_file(&style, path) {
                 Ok(ref s) if args.flag_inplace => {
                     let mut file = File::create(path).unwrap();
                     write!(file, "{}", s).unwrap();
                 },
                 Ok(ref s)  => write!(&mut stdout(), "{}", s).unwrap(),
-                Err(ref s) => write!(&mut stderr(), "{}", s).unwrap(),
+                Err(ref s) => {
+                    write!(&mut stderr(), "{}", s).unwrap();
+                    std::env::set_exit_status(1);
+                },
             }
         }
     }
 }
 
-fn format_source_file(style: FormatStyle, path: &Path) -> Result<String, String> {
+fn format_source_file(style: &FormatStyle, path: &Path) -> Result<String, String> {
     let source = {
         let mut file = match File::open(path) {
             Err(_) => return Err(format!("Couldn't open file: {}", path.display())),
@@ -78,17 +86,17 @@ fn format_source_file(style: FormatStyle, path: &Path) -> Result<String, String>
         }
     };
 
-    let replacements = rustfmt::reformat(&source, &style);
+    let replacements = rustfmt::reformat(&source, style);
     Ok(Replacement::apply_all(&replacements, &source))
 }
 
-fn format_stdin(style: FormatStyle) -> Result<String, String> {
+fn format_stdin(style: &FormatStyle) -> Result<String, String> {
     let mut source = String::new();
     match stdin().read_to_string(&mut source) {
         Err(ref e) => return Err(format!("Failed to read stdin: {}", Error::description(e))),
         Ok(_) => {},
     }
 
-    let replacements = rustfmt::reformat(&source, &style);
+    let replacements = rustfmt::reformat(&source, style);
     Ok(Replacement::apply_all(&replacements, &source))
 }
