f299d04e676f75bba0fb7f1c07eaaac51e66f8da
Kyle Pink <kylepink@gmail.com>
2015-04-26 00:17:59 +0930

    Reset interactive mode commit (readline crate did not support windows).

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
 #![feature(exit_status)]
-#![cfg(not(test))]
-extern crate readline;
+
 extern crate docopt;
 extern crate rustc_serialize;
 extern crate rustfmt;
-
-use readline::readline;
-
-use std::ffi::CString;
 
 use docopt::Docopt;
 use rustc_serialize::json;
@@ -18,7 +13,6 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::{stdin, stdout, stderr};
 use std::path::Path;
-use std::str;
 
 static USAGE: &'static str = "
 Overview: A tool to format rust code.
@@ -29,14 +23,13 @@ If no file arguments are specified, input is read from standard input.
 
 Usage: rustfmt [-w] [<file>]...
        rustfmt [--output-replacements-json] [<file>]...
-       rustfmt (--help | --version | --interactive)
+       rustfmt (--help | --version)
 
 Options:
-    -h, --help                      Show this message
-    -w, --write                     Overwrite the input files
-    -V, --version                   Print version info and exit
-    -j, --output-replacements-json  Outputs replacements as JSON
-    -i, --interactive               Interactive Mode
+    -h, --help                  Show this message
+    -w, --write                 Overwrite the input files
+    -V, --version               Print version info and exit
+    --output-replacements-json  Outputs replacements as JSON
 ";
 
 #[derive(RustcDecodable, Debug)]
@@ -44,66 +37,35 @@ struct Args {
     arg_file: Vec<String>,
     flag_write: bool,
     flag_output_replacements_json: bool,
-    flag_interactive: bool,
 }
 
 #[allow(dead_code)]
 fn main() {
     let debug = if cfg!(debug_assertions) { " debug" } else { "" };
     let version = format!("rustfmt version {}{}", env!("CARGO_PKG_VERSION"), debug);
-
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.version(Some(version)).help(true).decode())
         .unwrap_or_else(|e| e.exit());
 
     let style = FormatStyle::default();
 
-    match get_actions(&args) {
-        Mode::FileBased(actions) => {
-            for action in &actions {
-                match perform_input(action) {
-                    Ok(ref source) => {
-                        let replacements = reformat(source, &style);
-                        perform_output(action, &args, source, &replacements);
-                    },
-                    Err(ref msg) => {
-                        write!(&mut stderr(), "{}", msg).unwrap();
-                        std::env::set_exit_status(1);
-                    },
-                }
-            }
-        }
-        Mode::Interactive(ref action) => {
+    let actions = get_actions(&args);
 
-            let prompt_first_line = CString::new(">>> ").unwrap();
-            let prompt_additional_lines = CString::new("    ").unwrap();
-
-            while let Ok(source) = readline(&prompt_first_line) {
-                let mut source = str::from_utf8(source.to_bytes()).unwrap().to_string();
-                while let Ok(additional_line) = readline(&prompt_additional_lines) {
-                    let additional_line = str::from_utf8(additional_line.to_bytes()).unwrap();
-                    if additional_line.is_empty() {
-                        break;
-                    }
-                    source.push_str(additional_line);
-                }
-
-                if !source.is_empty() {
-                    println!("---formatted---\n");
-                    let replacements = reformat(&source, &style);
-                    perform_output(action, &args, &source, &replacements);
-                    println!("\n");
-                }
-            }
+    for action in &actions {
+        match perform_input(action) {
+            Ok(ref source) => {
+                let replacements = reformat(source, &style);
+                perform_output(action, &args, source, &replacements);
+            },
+            Err(ref msg) => {
+                write!(&mut stderr(), "{}", msg).unwrap();
+                std::env::set_exit_status(1);
+            },
         }
     }
 }
 
-fn get_actions(args: &Args) -> Mode {
-    if args.flag_interactive {
-        return Mode::Interactive(Action {input: Input::StdIn, output: Output::StdOut });
-    }
-
+fn get_actions(args: &Args) -> Vec<Action> {
     let mut actions = Vec::new();
 
     if args.arg_file.is_empty() {
@@ -118,12 +80,8 @@ fn get_actions(args: &Args) -> Mode {
             actions.push(Action { input: Input::File(file), output: output });
         }
     }
-    Mode::FileBased(actions)
-}
 
-enum Mode <'a>{
-    FileBased(Vec<Action<'a>>),
-    Interactive(Action<'a>),
+    actions
 }
 
 struct Action<'a> {
